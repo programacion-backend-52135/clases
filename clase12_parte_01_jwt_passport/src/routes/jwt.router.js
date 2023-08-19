59dd3b9050bef655a1309f97a10e4dd71e5eed5c
Arturo Verbel <arturo-verbel@hotmail.com>
2023-08-19 10:21:20 -0500

    Update..

--- clase12_parte_01_jwt_passport/src/routes/jwt.router.js
+++ clase12_parte_01_jwt_passport/src/routes/jwt.router.js
@@ -2,21 +2,35 @@ import { Router } from "express";
 import { generateToken, authToken } from "../utils.js";
 
 const usersDB = [
-    {email: 'dario@gmail.com', password: '123456', name: 'Dario'},
+    { 
+        email: 'dario@gmail.com', 
+        password: '123456', 
+        name: 'Dario' 
+    },
 ]
 const router = Router()
 
 router.post('/register', (req, res) => {
     const user = req.body
 
-    if( usersDB.find(u => u.email === user.email)) {
+    if (usersDB.find(u => u.email === user.email)) {
         return res.status(400).send('User already exits')
     }
 
     usersDB.push(user)
     const access_token = generateToken(user)
 
-    res.send({status: 'success', access_token })
+    res.send({ status: 'success', access_token })
+})
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+
+    const user = usersDB.find(u => u.email === email && u.password === password)
+    if(!user) return res.status(401).send({status: "error", error: 'Invalid pass'})
+
+    const access_token = generateToken(user)
+    res.send({ status: 'success', access_token })
 })
 
 router.get('/current', authToken, (req, res) => {
